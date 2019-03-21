9d0a376fe4689be32d563e57fbe6e6bb602176a1
linh4 <linh44h@gmail.com>
2019-03-21 17:53:06 -0400

    Updates

---
+++ precache-manifest.5c3b7cb9016b543166af4caed6f04846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d57fed49f4f68e20a41b",
+    "url": "/webnow-landing-page/static/js/runtime~main.8fd99e85.js"
+  },
+  {
+    "revision": "659ab36ecfff4f0868e1",
+    "url": "/webnow-landing-page/static/js/main.fd7cad8d.chunk.js"
+  },
+  {
+    "revision": "d7048cfa20afdbbe0efe",
+    "url": "/webnow-landing-page/static/js/2.a743c3f2.chunk.js"
+  },
+  {
+    "revision": "659ab36ecfff4f0868e1",
+    "url": "/webnow-landing-page/static/css/main.8caa17f2.chunk.css"
+  },
+  {
+    "revision": "3011a2d739ba012f0f6837d3649b2a7b",
+    "url": "/webnow-landing-page/index.html"
+  }
+];
